130dbc9dd2155a68472f1bac2de8b88deff4b647
EralyRempel <251251@astanait.edu.kz>
2026-05-23 13:40:57 +0500

    Edge.java

--- Assignment 4/src/Edge.java
+++ Assignment 4/src/Edge.java
@@ -1,13 +1,25 @@
+// Bonus
 public class Edge {
-    private Vertex source; // [cite: 26]
-    private Vertex destination; // [cite: 28]
+    private Vertex source;
+    private Vertex destination;
+    private int weight;
 
-    public Edge(Vertex source, Vertex destination) { // [cite: 31]
+    public Edge(Vertex source, Vertex destination) {
         this.source = source;
         this.destination = destination;
+        this.weight = 1;
     }
-    public Vertex getSource() { return source; } // [cite: 32]
-    public Vertex getDestination() { return destination; } // [cite: 32]
+
+    public Edge(Vertex source, Vertex destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public Vertex getSource() { return source; }
+    public Vertex getDestination() { return destination; }
+    public int getWeight() { return weight; }
+
     @Override
-    public String toString() { return source + " -> " + destination; } // [cite: 33]
+    public String toString() { return source + " -> " + destination + " [" + weight + "]"; }
 }
